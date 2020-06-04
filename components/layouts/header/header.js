b8da7bf04d51ab3cc0d4d9cd272ba71abe5136a9
dongql24083 <dongql24083@hundsun.com>
2020-06-04 20:09:57 +0800

    提交本地代码

---
+++ components/layouts/header/header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+class MyHeader extends React.Component {
+
+  render () {
+    return (
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+      </Layout>
+    )
+  }
+}
+export default MyHeader
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // import { Layout, Menu, Breadcrumb } from 'antd';
 // import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import React from 'react'
-
+import Header from '../components/layouts/header/header.js'
 // const { SubMenu } = Menu;
 // const { Header, Content, Footer, Sider } = Layout;
 
 class App extends React.Component {
 
   render () {
-    return (<div>11</div>)
+    return (<Header></Header>)
   }
 }
 
