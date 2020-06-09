9a8cec8ccef6fca89f06e4aee249843eacca23a8
along <120074791@qq.com>
2020-06-10 00:17:52 +0800

    本地代码提交

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
+let root = document.getElementById("root");
+let height = document.body.offsetHeight;
+let width = document.body.offsetWidth;
+console.log(root.style);
+console.log(height, width);
+root.style.height = height;
+root.style.width = width;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
